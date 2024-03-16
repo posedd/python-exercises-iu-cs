e2129bcaefb2064cd5610167778d8ca5a2985d5d
posedd <ozgraltg@gmail.com>
2024-03-16 12:11:14 +0300

    Python iu cs lab exercises.

--- week2/s4.py
+++ week2/s4.py
@@ -2,10 +2,10 @@
 # Kelimeler boşluklarla ayrılır ve noktalama işaretleri yok sayılır.
 
 text = input("Bir metin giriniz: ")
-new_text=text.replace(",", " ").replace(".", " ").replace("?", " ").replace("!", " ").split()
+new_text = text.replace(",", " ").replace(".", " ").replace("?", " ").replace("!", " ").split()
 kelime_sayisi = len(new_text)
-en_uzun=(new_text[0])
+en_uzun = (new_text[0])
 for kelime in new_text:
     if len(kelime) >= len(en_uzun):
-        en_uzun=kelime
+        en_uzun = kelime
 print(f"En uzun kelime: {en_uzun}")
